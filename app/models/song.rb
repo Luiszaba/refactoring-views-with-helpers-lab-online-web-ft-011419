08898e2ce08cc864db8702cae85f9a375708fbbd
Luis Zabala <luis.zabala076@gmail.com>
2019-04-02 00:47:39 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist_name
   end
-
-  def artist_name=(name)
+  
+  def artist_name=(artist_name)
+    self.artist = Artist.find_by_or_create(name: artist_name)
   end
 end
